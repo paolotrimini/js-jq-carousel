0bc593261dd6f0d9b414af9b7cdd253bc5bf6b8b
paolotrimini <paolo.trimini@gmail.com>
2021-03-22 13:31:32 +0100

    impostazione funzioni

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+// Generare, tramite utilizzo di Jquery, lo slider immagini.
+
+
+
+function nextClick() {      // img successiva
+
+}
+
+function prevClick() {      // img precedente
+
+}
+
+
+function init() {
+    $('.next').click(nextClick);
+    $('.prev').click(prevClick);
+
+}
+$(document).ready(init);
