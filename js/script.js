e5c91a6c9362bb299c708dea76e48c06a468eddd
paolotrimini <paolo.trimini@gmail.com>
2021-03-22 15:07:59 +0100

    next img

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 // Creare uno slider di immagini (JQ)
 
-
+// Soluzione 1
 
 function prevClick(){
-    console.log('test img precedente');
+    //console.log('test img precedente');
 
 }
 
 function nextClick(){
     console.log('test img successiva');
 
+    var activeImg = $('.active');               // immag. corrente
+    var nextImg = activeImg.next('img');  // "attivo" immag. successiva
+
+    activeImg.removeClass('active');        // rimuovo classe immag. corrente
+    nextImg.addClass('active');             // "consegno" la classe a immag. successiva
 
 }
 
@@ -20,3 +25,7 @@ function init() {
 }
 
 $(document).ready(init);
+
+
+
+// Soluzione 2
